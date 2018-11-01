d47dd033226ee63eb3476d8a331a7adf1841e11b
Javier Alejandro Acevedo Barroso <jaalacba@hotmail.com>
2018-11-01 15:00:35 -0500

    to Cluster

--- simPlots3D.py
+++ simPlots3D.py
@@ -13,21 +13,31 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 dpiT = 300
 
-def fastShow(image, title="none",clim=None, clabel=None):
+
+constantes = np.loadtxt("./datFiles/constants.dat", usecols = 1)
+#print(constantes)
+tamano = int(constantes[14])
+Nt = int(constantes[-1])
+wMin = constantes[-2]
+
+def darV(i):
+    return wMin+tamano*i
+
+def fastShow(image, title="none",clim=None, clabel=None, saveN=None):
     plt.clf()
     plt.imshow(image,interpolation='nearest', aspect='auto')
     if(clim != None):
         plt.clim(clim[0],clim[1])
-#    cbar = plt.colorbar()
+    cbar = plt.colorbar()
 #    if(clabel != None):
 #        cbar.set_label(clabel)
     plt.yticks(plt.yticks()[0], [str(np.round(t*473,3)) for t in plt.yticks()[0]]) 
     plt.ylabel("Velocity [km/s]")
-    plt.xticks(plt.xticks()[0], [str(t*200) for t in plt.xticks()[0]])
+    plt.xticks(plt.xticks()[0], [str(t*20) for t in plt.xticks()[0]])
     plt.xlabel("Position [kpc]")
     plt.title(title)	
     print("SATAN")
-    plt.savefig("./images/"+title+".png",dpi=dpiT)
+    plt.savefig("./images/"+saveN+".png",dpi=dpiT)
 
 
 
@@ -37,10 +47,7 @@ def fmt(x, pos):
     b = int(b)
     return r'${} \times 10^{{{}}}$'.format(a, b)
 
-constantes = np.loadtxt("./datFiles/constants.dat", usecols = 1)
-#print(constantes)
-tamano = int(constantes[14])
-Nt = int(constantes[-1])
+
 
 
 
@@ -53,7 +60,7 @@ densidadXZ0  = np.loadtxt('./datFiles/densXZ0.dat').T
 #densidadXZ1  = np.loadtxt('./datFiles/densXZ1.dat').T
 
 
-fastShow(densidadXY0, title="Corte Z = 0 de la densidad. Nt = {:d}".format(0),clim=[0,0.016],clabel="Mass density [$M_{\odot}$ / kp$c^3$]" )
+fastShow(densidadXY0, title="Corte Z = 0 de la densidad. Nt = {:d}".format(0),clim=[0,0.016],clabel="Mass density [$M_{\odot}$ / kp$c^3$]",saveN="XY0" )
 fastShow(densidadYZ0, "YZ0")
 fastShow(densidadXZ0, "XZ0")
 
@@ -64,14 +71,13 @@ fastShow(densidadXZ0, "XZ0")
 #plt.figure()
 imagenes = tamano//8
 for i in range(0,tamano,tamano//imagenes):
-    potXY0 = np.loadtxt("./datFiles/pot0XY{:d}.dat".format(i)).T
+#    potXY0 = np.loadtxt("./datFiles/pot0XY{:d}.dat".format(i)).T
     #potXY1 = np.loadtxt("./datFiles/pot1XY{:d}.dat".format(i)).T
-    #diff = potXY0 - potXY1
-    accexXY0 = np.loadtxt("./datFiles/accex0XY{:d}.dat".format(i)).T
+ #   accexXY0 = np.loadtxt("./datFiles/accex0XY{:d}.dat".format(i)).T
     #accexXY1 = np.loadtxt("./datFiles/accex1XY{:d}.dat".format(i)).T
-    #diff1 = accexXY0 -accexXY1
-    fastShow( potXY0 , "potXY{:d}".format(i) )
-    fastShow( accexXY0 , "accex0XY{:d}".format(i) )
+
+#    fastShow( potXY0 , "potXY{:d}".format(i) )
+    #fastShow( accexXY0 , "accex0XY{:d}".format(i) )
    # fastShow( accexXY1 , "accex1XY{:d}".format(i) )
     
 
@@ -82,7 +88,7 @@ for i in range(1,Nt):
 	#densidadYZ = np.loadtxt('./datFiles/densYZ{:d}.dat'.format(i)).T
 	#densidadXZ = np.loadtxt('./datFiles/densXZ{:d}.dat'.format(i)).T
 
-	fastShow(densidadXY,title="Corte Z = 0 de la densidad. Nt = {:d}".format(i), clim=[0,0.016],clabel="Mass density [$M_{\odot}$ / kp$c^3$]")
+	fastShow(densidadXY,title="Corte Z = 0 de la densidad. Nt = {:d}".format(i), clim=[0,0.016],clabel="Mass density [$M_{\odot}$ / kp$c^3$]",saveN="XY{:d}".format(i) )
 	#fastShow(densidadYZ, "YZ{:d}".format(i))
 	#fastShow(densidadXZ, "XZ{:d}".format(i))
 
