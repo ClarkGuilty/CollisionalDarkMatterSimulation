bed19aa5821989e6578a600546c9923a0fe86d94
Javier Alejandro Acevedo Barroso <ja.acevedo12@uniandes.edu.co>
2018-11-18 18:40:32 -0500

    to Cluster

--- simulacion3D.c
+++ simulacion3D.c
@@ -69,7 +69,7 @@ Simulación del espacio de fase de un fluido tridimensional de Materia Oscura co
 
 //Aspectos temporales de la ejecución.
 double dt = 0.5;
-int Nt = 25;
+int Nt = 10;
 
 //Arreglos
 double *phase;
@@ -222,7 +222,7 @@ void calMacro();
 
 double sr = 0.1;
 double sv = 0.1;
-double ampl = 80;
+double ampl = 90;
 
 
 int main()
@@ -267,6 +267,7 @@ int main()
 	printConstant("Nvy",Nvy);
     printConstant("Nvz",Nvz);
 	printConstant("Nt", Nt);
+    //printConstant("Tau", TAU);
     printf("%f %f %f %d %d %d\n", sr,sv,ampl,Nvx,Nvy,Nvz);
 
 
